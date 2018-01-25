246abbd803677d1591442b1d85a6d5fc8b9ef03e
herbly <cherish1990ly@163.com>
2018-01-25 17:33:13 +0800

    响应式静态页面完成

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+
+// owl 轮播
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
